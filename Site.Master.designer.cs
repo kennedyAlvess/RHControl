579d95d80efb47365338d23a451567fab230e543
kennedyAlvess <kennedy.alves@csjsistemas.com.br>
2025-05-10 12:21:13 -0300

    Feat: sidebar inicial

--- Site.Master.designer.cs
+++ Site.Master.designer.cs
@@ -15,6 +15,15 @@ namespace RHControl
     {
 
         /// <summary>
+        /// Controle HeadContent.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder HeadContent;
+
+        /// <summary>
         /// Controle MainContent.
         /// </summary>
         /// <remarks>
